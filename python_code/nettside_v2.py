5f0653b4044fdf74d7cf8aa46d7b0704937563a9
JakobFE04 <eikeland.jakob@gmail.com>
2026-02-23 09:34:02 +0100

    Nettside oppdatering

--- python_code/nettside_v2.py
+++ python_code/nettside_v2.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python
 from flask import Flask, render_template
-import time
-import serial
-import pynmea2
-import gps_to_csv_call
+import nettside_data
 
-ser = serial.Serial(
-        port='/dev/ttyAMA0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
+#ser = serial.Serial(
+#         port='/dev/ttyAMA0',
+#         baudrate = 9600,
+#         parity=serial.PARITY_NONE,
+#         stopbits=serial.STOPBITS_ONE,
+#         bytesize=serial.EIGHTBITS,
+#         timeout=1
+# )
 
-#def get_data():
-#     x=ser.readline().decode('utf-8', errors='ignore')
-#     if x.startswith('$'):
-#                try:
-#         msg = pynmea2.parse(x)
-#                       if isInstance(msg, pynmea2.RMC): # and msg.status == 'A                               
-#         print('-'*20)
-#         print(msg.timestamp)
-#         print(msg.latitude, msg.lat_dir)
-#         print(msg.longitude, msg.lon_dir)
-#         print(msg.speed_kph)
-#         print('-'*20)
-#                except pynmea2.ParseError as e:
-#			print(f"ParseError")
-#                except Exception as e:
-#			print(f"Error {e}")
-#
-#     time.sleep(0.1)
-#     return {"Time": msg.timestamp, "Latitude": msg.latitude, "Longitude": msg.longitude}
+# #def get_data():
+#      x=ser.readline().decode('utf-8', errors='ignore')
+#      if x.startswith('$'):
+# #                try:
+#          msg = pynmea2.parse(x)
+# #                       if isInstance(msg, pynmea2.RMC): # and msg.status == 'A                               
+#          print('-'*20)
+#          print(msg.timestamp)
+#          print(msg.latitude, msg.lat_dir)
+#          print(msg.longitude, msg.lon_dir)
+#          print(msg.speed_kph)
+#          print('-'*20)
+# #                except pynmea2.ParseError as e:
+# #			print(f"ParseError")
+# #                except Exception as e:
+# #			print(f"Error {e}")
+
+#      time.sleep(0.1)
+#      return {"Time": msg.timestamp, "Latitude": msg.latitude, "Longitude": msg.longitude}
 
 
-gps = gps_to_csv_call.get_gps()
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-	data = {"Latitude": gps[1][0], "Longitude":gps[1][1], "Velocity":gps[1][2]} 
+	data = nettside_data.get_gps()
 	return render_template('index.html', measurements=data)
 
 if __name__ == '__main__':
