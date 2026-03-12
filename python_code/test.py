0ba0375f8928df2f85a76ae4e1a1da72a6c0b57b
JakobFE04 <eikeland.jakob@gmail.com>
2026-03-12 14:52:25 +0100

    acc test

--- python_code/test.py
+++ python_code/test.py
@@ -1,7 +1,11 @@
-from bottle import route, run
+import vel_pos
 
-@route('/hello')
-def hello():
-	return "Hello there..."
-
-run(host='192.168.8.39', port=80, debug=True, reloader=True)
+while True:
+	vel, pos, yaw_ang, time, yaw_deg = vel_pos.vel_pos(vel, pos, yaw_ang, time)
+	print("-"*30)
+	print(pos)
+	print("-"*30)
+	print(vel)
+	print("-"*30)
+	print(yaw_deg)
+	print("-"*30)
