ee71f57a3c57fc4c295ef39099bf4594277a55b8
JakobFE04 <eikeland.jakob@gmail.com>
2026-04-04 18:18:41 +0200

    please

--- Navmain/Main2.py
+++ Navmain/Main2.py
@@ -110,7 +110,7 @@ def les_sensorer_og_kalman():
     """Henter bilens estimerte posisjon i meter (X,Y) og retning."""
     # Henter rå-GPS fra modulen din
     pos = gps_to_csv_call.get_gps()
-    if pos[0] != 0 and pos[1 != 0]:
+    if pos[1][0] != 0 and pos[1][1] != 0:
         raw_lon = pos[1][1]
         raw_lat = pos[1][0]
         gps_x, gps_y = lon_lat_til_xy(raw_lon, raw_lat)
