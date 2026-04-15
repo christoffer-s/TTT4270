c02dabd74f51f9f07808a371c6f0ff5e85b0acbf
JakobFE04 <eikeland.jakob@gmail.com>
2026-04-15 15:28:02 +0200

    motorer..---.----

--- Navmain/motor_ctrl.py
+++ Navmain/motor_ctrl.py
@@ -27,11 +27,11 @@ class Motor:
         self.pwm.value = 0
 
 
-""" right_motor = Motor(6, 12) #Koble GPIO 6 til pin 4 på motor driver board, og GPIO 12 til pin 3
+right_motor = Motor(6, 12) #Koble GPIO 6 til pin 4 på motor driver board, og GPIO 12 til pin 3
 left_motor = Motor(16, 13) #Koble GPIO 16 til pin 8 på motor driver board, og GPIO 13 til pin 5
 back_left_motor = Motor(17, 18) #Koble GPIO 17 til pin 12 på motor driver board, og GPIO 18 til pin 11
 back_right_motor = Motor(26, 19) #Koble GPIO 26 til pin 7 på motor driver board, og GPIO 19 til pin 6
- """
+
 class Drive:
     def __init__(self, right_motor, left_motor, back_left_motor, back_right_motor):
         self.right_motor = right_motor
@@ -89,4 +89,4 @@ class Drive:
         self.back_right_motor.stop()
             
 
-""" drive = Drive(right_motor, left_motor, back_left_motor, back_right_motor) """
+drive = Drive(right_motor, left_motor, back_left_motor, back_right_motor)
